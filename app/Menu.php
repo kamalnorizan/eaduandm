7761f3a45e69098e99587d0639a571fe986f6270
Kamal Norizan <kamalnorizan@gmail.com>
2019-11-04 15:25:25 +0800

    Passport Installed

--- app/Menu.php
+++ app/Menu.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     //
+    public $timestamps = true;
+
+    protected $table = 'menu';
+
+    protected $primaryKey = 'menu_id';
+
+    public $incrementing = true;
+
+    // protected $fillable = ['fillable'];
+    protected $guarded = ['menu_id'];
 }
--- app/Pelajar.php
+++ app/Pelajar.php
@@ -3,8 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
-class Pelajar extends Model
+use Illuminate\Foundation\Auth\User as Authenticable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+class Pelajar extends Authenticable
 {
     //
+    use Notifiable, HasApiTokens;
+
+    protected $guard = 'pelajar';
+
+    public $timestamps = true;
+
+    protected $table = 'pelajar';
+
+    protected $primaryKey = 'ndp';
+
+    protected $hidden = [
+        'password'
+    ];
 }
